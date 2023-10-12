7936719d33c9aae5b7ba316e69d0fefbd42c5a9a
linh <linh1862002@gamil.com>
2023-10-12 16:44:06 +0100

    api-state

--- src/App.js
+++ src/App.js
@@ -4,10 +4,33 @@ import About from './About';
 import{
   BrowserRouter as Router,Switch,Route, Link} from 'react-router-dom';
 import Home from './Home';
+import React from 'react';
+import  ReactDOM  from 'react-dom';
 // import Contact from './Contact';
 
+class App extends React.Component{
+  constructor(){
+    super();
+    this.state = {
+      data: [],
+    };
+    this.setStateHandler = this.setStateHandler.bind(this);
+  }
+  setStateHandler = () => {
+    const item = 'setState...'
+    const myArray = this.state.data.slice();
+    myArray.push(item);
+    this.setState({data: myArray});
 
-function App() {
+  }
+  forceUpdateHandler = () => {
+    this.forceUpdate();
+  }
+  findDomNodeHandler = () => {
+    const myDiv = document.getElementById('myDiv');
+    ReactDOM.findDOMNode(myDiv).style.color = 'green';
+  }
+render() {
   return (
     <div className="App">
       <header className="App-header">
@@ -38,9 +61,22 @@ function App() {
             </Switch>
       		</div>
     	</Router>
+      <div>
+        <button onClick={this.setStateHandler}>SET STATE</button>
+        <h4>State Array: {this.state.data}</h4>
+      </div>
+      <div>
+        <button onClick={this.forceUpdateHandler}>SET STATE</button>
+        <h4>number: {Math.random()}</h4>
+      </div>
+      <div>
+        <button onClick={this.findDomNodeHandler}>Find</button>
+        <div id='myDiv'>Node</div>
+      </div>
       </header>
     </div>
   );
+}
 }
 
 export default App;
